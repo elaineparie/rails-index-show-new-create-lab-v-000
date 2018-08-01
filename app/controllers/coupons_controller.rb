ff9eaea4cd246bb1fcc48a239235960910ede604
Elaine Anderson <eparie@bu.edu>
2018-08-01 20:41:21 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,7 +1,12 @@
 class CouponsController < ActionController::Base
 
   def create
+    @coupon = Coupon.new
+@coupon.coupon_code = params[:coupon_code]
+@coupon.store = params[:store]
+@coupn.save
+
 
   end
-  
+
 end
